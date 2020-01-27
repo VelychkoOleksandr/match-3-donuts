7fd052d0cdcfe9d88a3435475cc0a7fa072c82c4
Oleksandr Velychko <49977771+VelychkoOleksandr@users.noreply.github.com>
2020-01-27 06:14:55 +0200

    Fixes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 import ManuState from 'states/ManuState';
+import GameState from './states/GameState';
+import ResultState from './states/ResultState';                 
 
-class Game extends Phaser.Game {
-
-	constructor() {
-		super(800, 600, Phaser.AUTO, 'content', null);
+const game_option = {
+	windowSize: {  
+		width: 800,
+		height: 750
+	}
+}     
+    
+class Game extends Phaser.Game {   
+                                                     
+	constructor() {                       	                
+		super(game_option.windowSize.width, game_option.windowSize.height, Phaser.AUTO, 'content', null);    
+  
 		this.state.add('ManuState', ManuState, false);
-		this.state.start('ManuState');
+		this.state.add('GameState', GameState, false); 
+		this.state.add('ResultState', ResultState, false);
+
+		this.state.start('ManuState');                      
+	}; 
+                        
+	playGame() {            
+		this.state.start('GameState');               	       	
 	}
 }
 
-new Game(); 
+new Game();
