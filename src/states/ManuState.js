09a349a58b01a7a8db77024d3778fbec633c599a
Oleksandr Velychko <49977771+VelychkoOleksandr@users.noreply.github.com>
2020-01-27 06:17:01 +0200

    Code optimisation

--- src/states/ManuState.js
+++ src/states/ManuState.js
@@ -1,13 +1,5 @@
 class ManuState extends Phaser.State {
 
-	constructor() {
-		super();
-		this.windowSize = {
-			width: 800,
-			height: 600
-		};
-	}
-
 	preload() {
 		this.load.image('background', './assets/images/backgrounds/background.jpg');
 		this.load.image('sfx', './assets/images/btn-sfx.png');
@@ -23,18 +15,18 @@ class ManuState extends Phaser.State {
 		//Set sfx button
 		const btn_sfx = this.add.sprite(0, 5, 'sfx');
 		btn_sfx.scale.setTo(0.75);
-		btn_sfx.x = this.windowSize.width - btn_sfx.width - 5; 
+		btn_sfx.x = this.game.width - btn_sfx.width - 5; 
 
 		//Set logo
 		const logo = this.add.sprite(0, 0, 'logo');
 		logo.scale.setTo(0.9);
-		logo.x = (this.windowSize.width / 2) - (logo.width / 2);
+		logo.x = (this.game.width / 2) - (logo.width / 2);
 		logo.y = logo.height * 0.5; 
 
 		//Set play button
 		const btn_play = this.add.sprite(0, 0, 'btn-play');
 		btn_play.scale.setTo(0.95);
-		btn_play.x = (this.windowSize.width / 2) - (btn_play.width / 2);
+		btn_play.x = (this.game.width / 2) - (btn_play.width / 2);
 		btn_play.y = logo.y + logo.height + 30;
 
 		//Click button event
